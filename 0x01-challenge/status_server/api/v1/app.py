ba493aa66089d90d39f8e16a1d5f190d13bc3ed6
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-19 13:52:57 +0200

    Fix_My_Code_Challenge->0x01-challenge

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -8,11 +8,13 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(404)
 def not_found(error):
     """ json 404 page """
     return make_response(jsonify({"error": "Not found"}), 404)
 
+
 if __name__ == "__main__":
-    # Run the app only if the script is executed directly, not when imported as a module
+    # python -m api.v1.app 
     app.run(host="0.0.0.0", port=5000)
